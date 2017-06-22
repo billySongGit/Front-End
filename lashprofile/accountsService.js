b3276ef66d70b8a043534e487fa0efce49c71b1f
Billy Song <billy.song12@gmail.com>
2017-06-22 15:44:33 -0700

    Add files via upload

---
+++ lashprofile/accountsService.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .factory('$accountsService', AccountsService);
+
+    AccountsService.$inject = ['$baseService', '$sabio'];
+
+    function AccountsService($baseService, $sabio) {
+        var accountsServiceObject = sabio.services.accounts;
+        var newService = $baseService.merge(true, {}, accountsServiceObject, $baseService);
+
+        return newService;
+    };
+})();
