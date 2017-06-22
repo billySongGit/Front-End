0c9c4cd69ae6d0048b2290340d96b4ab3703e72f
Billy Song <billy.song12@gmail.com>
2017-06-22 13:57:54 -0700

    Add files via upload

---
+++ service/accountRouteConfig.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+
+            $routeProvider.caseInsensitiveMatch = true;
+
+            $locationProvider.hashPrefix('!');
+
+            $routeProvider.when('/', {
+                templateUrl: '/Scripts/app/account/template/accountPage.html',
+                controller: 'accountWizardController',
+                controllerAs: 'accountController'
+            }).when('/password', {
+                templateUrl: '/Scripts/app/account/template/passwordPage.html',
+                controller: 'accountPasswordController',
+                controllerAs: 'passwordController'
+            }).when('/settings', {
+                templateUrl: '/Scripts/app/account/template/settingsPage.html',
+                controller: 'settingsController',
+                controllerAs: 'sc'
+            });
+            $locationProvider.html5Mode({
+                enabled: false,
+                requireBase: false
+            });
+        }]);
+})();
